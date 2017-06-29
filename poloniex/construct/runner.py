8d3432d1cd927ab7831508a2a284e94df088e404
Brennan Morell <brennanmorell@Brennans-MacBook-Pro.local>
2017-06-28 21:52:49 -0500

    Refactor wizard.py and create designated runner

---
+++ poloniex/construct/runner.py
@@ -0,0 +1,9 @@
+from wizard import Wizard
+
+btc_wizard = Wizard('BTC_ETH', 10)
+btc_etc = Wizard('BTC_ETC', 10)
+eth_etc = Wizard('ETH_ETC', 10)
+
+btc_wizard.start_book('BTC_ETH', 10)
+
+ 
--- poloniex/construct/wizard.py
+++ poloniex/construct/wizard.py
@@ -1,3 +1,4 @@
+
 from poloniex.settings import POLONIEX_DIR
 from poloniex.api.api import PoloniexAPI
 from poloniex.model.wizard_build import Tick, BidBook, AskBook, TradeBook
@@ -16,19 +17,21 @@ class Wizard(object):
 	def __init__(self, pair, depth):
 		self.pair = pair
 		print('BOOK: Starting book for pair: {0}'.format(self.pair))
-
 		market_orders = PoloniexAPI().marketOrders(self.pair, depth)
 		self.bid_book = BidBook(depth, market_orders['bids'])
 		print('BOOK: bid_book populated with public API marketOrders() call')
 		self.ask_book = AskBook(depth, market_orders['asks'])
 		print('BOOK: ask_book populated with public API marketOrders() call')
-
 		market_trade_history = PoloniexAPI().marketTradeHist(self.pair)
 		self.trade_book = TradeBook(depth, market_trade_history)
 		print('BOOK: trade_book populated with public API marketTradeHist() call')
 		self._tickerP = Popen(["python", POLONIEX_DIR['stream'] + 'wizard_streamer.py', self.pair], stdout=PIPE, bufsize=1)
 		print('BOOK: polo_streamer.py subprocess started')
 
+	def start_book(self, pair, depth):
+		"""
+		Starts the ticker subprocess
+		"""
 		self._tickerT = Thread(target=self.catch_book)
 		self._tickerT.daemon = True
 		self._tickerT.start()
@@ -80,27 +83,27 @@ class Wizard(object):
 # 			for tick_str in iter(test_popen.stdout.readline, b''):
 # 				print(tick_str)
 
-if __name__ == "__main__":
-	btc_eth = Wizard('BTC_ETH', 10)
-	btc_etc = Wizard('BTC_ETC', 10)
-	eth_etc = Wizard('ETH_ETC', 10)
+#if __name__ == "__main__":
+	#btc_eth = Wizard('BTC_ETH', 10)
+	#btc_etc = Wizard('BTC_ETC', 10)
+	#eth_etc = Wizard('ETH_ETC', 10)
 
-	while True:
-		first_leg = btc_eth.ask_book.min_rate_level()[0]
+	#while True:
+		#first_leg = btc_eth.ask_book.min_rate_level()[0]
 		# print 'FIRST LEG: ' + str(first_leg)
-		second_leg = btc_etc.ask_book.min_rate_level()[0]
+		#second_leg = btc_etc.ask_book.min_rate_level()[0]
 		# print 'SECOND LEG: ' + str(second_leg)
-		third_leg = eth_etc.bid_book.max_rate_level()[0]
+		#third_leg = eth_etc.bid_book.max_rate_level()[0]
 		# print 'THIRD LEG: ' + str(third_leg)
 		# print (first_leg * third_leg / second_leg) * (.9975 * .9975 * .9975)
-		inverse_first_leg = eth_etc.ask_book.min_rate_level()[0]
+		#inverse_first_leg = eth_etc.ask_book.min_rate_level()[0]
 		# print 'INVERSE FIRST LEG: ' + str(inverse_first_leg)
-		inverse_second_leg = btc_etc.bid_book.max_rate_level()[0]
+		#inverse_second_leg = btc_etc.bid_book.max_rate_level()[0]
 		# print 'INVERSE SECOND LEG: ' + str(inverse_second_leg)
-		inverse_third_leg = btc_eth.bid_book.max_rate_level()[0]
+		#inverse_third_leg = btc_eth.bid_book.max_rate_level()[0]
 		# print 'INVERSE THIRD LEG: ' + str(inverse_third_leg)
-		print ("BTC-ETH MIN ASK: " + str(first_leg))
-		print ("BTC-ETC MIN ASK: " + str(second_leg))
+		#print ("BTC-ETH MIN ASK: " + str(first_leg))
+		#print ("BTC-ETC MIN ASK: " + str(second_leg))
 		# print ("BTC-ETH MIN ASK: " + str(first_leg))
 		# print (inverse_first_leg * inverse_third_leg / inverse_second_leg) * (.9975 * .9975 * .9975)
-		time.sleep(.5)
+		#time.sleep(.5)
